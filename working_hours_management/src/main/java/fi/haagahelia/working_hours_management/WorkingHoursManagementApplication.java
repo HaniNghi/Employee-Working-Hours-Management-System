52344e1a78d1f3958f0b694ff62445b37a6a4089
NghiVo <vmmn27@gmail.com>
2025-11-01 19:52:31 +0200

    First commit

---
+++ working_hours_management/src/main/java/fi/haagahelia/working_hours_management/WorkingHoursManagementApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.working_hours_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingHoursManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingHoursManagementApplication.class, args);
+	}
+
+}
---
+++ working_hours_management/src/test/java/fi/haagahelia/working_hours_management/WorkingHoursManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.working_hours_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingHoursManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
