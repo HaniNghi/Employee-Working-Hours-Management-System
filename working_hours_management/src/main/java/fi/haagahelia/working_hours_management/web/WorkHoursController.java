b5b18b8190407bada3c97a8444212c0e525ae8c5
NghiVo <vmmn27@gmail.com>
2025-11-08 20:16:40 +0200

    feat(work-hours): add weekly work schedule display for employees

--- working_hours_management/src/main/java/fi/haagahelia/working_hours_management/web/WorkHoursController.java
+++ working_hours_management/src/main/java/fi/haagahelia/working_hours_management/web/WorkHoursController.java
@@ -1,12 +1,18 @@
 package fi.haagahelia.working_hours_management.web;
 
+import java.time.DayOfWeek;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import fi.haagahelia.working_hours_management.domain.Employee;
 import fi.haagahelia.working_hours_management.domain.EmployeeRepository;
 import fi.haagahelia.working_hours_management.domain.ManagerRepository;
 import fi.haagahelia.working_hours_management.domain.WorkHour;
@@ -41,9 +47,31 @@ public class WorkHoursController {
         return "worklist";
     }
 
-    @GetMapping("/workhour")
-    @ResponseBody
-    public List<WorkHour> getAllWorkHours() {
-        return workHoursService.getAllWorkHours();
+    // @GetMapping("/workhour")
+    // @ResponseBody
+    // public List<WorkHour> getAllWorkHours() {
+    //     return workHoursService.getAllWorkHours();
+    // }
+
+    @RequestMapping("/workhour")
+    public String workhour(Model model) {
+        List<WorkHour> workHours = workHoursService.getAllWorkHours();
+
+    Map<Employee, Map<String, WorkHour>> workMap = new LinkedHashMap<>();
+
+        workHours.stream()
+        .collect(Collectors.groupingBy(WorkHour::getEmployee))
+        .forEach((employee, whList) -> {
+            Map<String, WorkHour> dayMap = whList.stream()
+                    .collect(Collectors.toMap(
+                            w -> w.getDate().getDayOfWeek().name(), // key là "MONDAY", "TUESDAY", ...
+                            w -> w
+                    ));
+            workMap.put(employee, dayMap);
+        });
+
+        model.addAttribute("workMap", workMap);
+        return "workhour";
     }
+
 }
